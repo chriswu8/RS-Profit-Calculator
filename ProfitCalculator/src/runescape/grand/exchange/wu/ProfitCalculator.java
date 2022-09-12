d1e2198918006cb5475a5d85e7c9205949f0fa01
Chris Wu <zezemint@gmail.com>
2022-09-11 23:50:17 -0700

    created ProfitCalculator class

---
+++ ProfitCalculator/src/runescape/grand/exchange/wu/ProfitCalculator.java
@@ -0,0 +1,14 @@
+package runescape.grand.exchange.wu;
+
+
+/**
+ * The ProfitCalculator calculates the rate of profit, the amount of Runescape gold points (GPs) per hour
+ */
+public class ProfitCalculator
+{
+    public ProfitCalculator()
+    {
+        
+    }
+
+}
